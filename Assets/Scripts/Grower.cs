6293221dbf8233c6b5bb3b83bd94552fc5e77ec7
Vix Sak <sakv@shernet.sheridancollege.ca>
2026-03-17 10:52:28 -0400

    set up a tree and apple that grows with a coroutine

--- Assets/Scripts/Grower.cs
+++ Assets/Scripts/Grower.cs
@@ -1,12 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class Grower : MonoBehaviour
 {
     public Transform treeTransform;
+    public Transform appleTransform;
+    public float applet = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         treeTransform.localScale = Vector2.zero;
+        appleTransform.localScale = Vector2.zero;
+        StartCoroutine(GrowTree());
 
     }
 
@@ -16,14 +21,33 @@ public class Grower : MonoBehaviour
         
     }
 
-    public void GrowTree() 
+    public void StartTreeGrowing() 
+    {
+        StartCoroutine(GrowTree());
+    }
+
+    IEnumerator GrowTree() 
     {
         float t = 0;
+        treeTransform.localScale = Vector2.zero;
+        appleTransform.localScale = Vector2.zero;
 
-        while (t > 1)
+        while (t < 1)
         {
             t += Time.deltaTime;
             treeTransform.localScale = Vector2.one * t;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(applet);
+
+        t = 0;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            appleTransform.localScale = Vector2.one * t;
+            yield return null;
         }
     }
 }
