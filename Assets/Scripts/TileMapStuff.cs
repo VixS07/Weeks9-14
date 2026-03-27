138277b926ed512814d1aaabc9691307a871135b
Vix Sak <sakv@shernet.sheridancollege.ca>
2026-03-27 11:44:59 -0400

    set up how to paint game objects

---
+++ Assets/Scripts/TileMapStuff.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class TileMapStuff : MonoBehaviour
+{
+    public Tilemap tilemap;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.)
+    }
+}
