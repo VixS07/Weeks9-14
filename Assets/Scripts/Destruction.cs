9495346a6499f65063568111c949d244c2b6d917
Vix Sak <sakv@shernet.sheridancollege.ca>
2026-03-31 11:40:31 -0400

    added a player which moves with a lerp and mouse click, and only moves when not clicking on a grass tile.

---
+++ Assets/Scripts/Destruction.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+
+public class Destruction : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(InputAction)
+
+    IEnumerator Crash()
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/DontWalkOnGrass.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEditor.Timeline;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class DontWalkOnGrass : MonoBehaviour
+{
+    public List<Vector2> movementPoints;
+
+    public Tilemap tilemap;
+    public Tile grass;
+    Vector2 newPos;
+    Vector3Int cell;
+
+    float t;
+    Coroutine lerpCR;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        movementPoints = new List<Vector2>();
+        movementPoints.Add(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+        cell = tilemap.WorldToCell(mousePos);
+        newPos = tilemap.GetCellCenterWorld(cell);
+    }
+
+    public void OnClick(InputAction.CallbackContext context) 
+    {
+        Debug.Log(context.phase);
+        if (context.performed)
+        {
+            if (tilemap.GetTile(cell) != grass)
+            {
+                t = 0;
+                if()
+                StartCoroutine(LerpMove());
+            }
+        }
+        
+     }
+
+    IEnumerator LerpMove()
+    {
+        Vector2 endPos = newPos;
+        Vector2 startPos = transform.position;
+        while (t <=1)
+        {
+            t += Time.deltaTime * 0.5f;
+
+            transform.position = Vector2.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+    }
+}
