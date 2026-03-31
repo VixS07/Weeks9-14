3fa6eccad3387835152ecf3e708bcd3664059eb9
Vix <vix.sak@gmail.com>
2026-03-30 22:39:35 -0400

    added a particle system, messed around with tilemaps, and added a cinemachine camera

--- Assets/Scripts/TileMapStuff.cs
+++ Assets/Scripts/TileMapStuff.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
@@ -5,6 +6,11 @@ using UnityEngine.Tilemaps;
 public class TileMapStuff : MonoBehaviour
 {
     public Tilemap tilemap;
+    public Transform highlight;
+
+    public Tile flower;
+
+    public CinemachineImpulseSource impulse;
     void Start()
     {
         
@@ -13,6 +19,20 @@ public class TileMapStuff : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.)
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector3Int cellPos = tilemap.WorldToCell(mousePos);
+        Vector3 pos = tilemap.GetCellCenterWorld(cellPos);
+
+        //Debug.Log(mousePos + " is at cell " + cellPos);
+        highlight.position = pos;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            //know what tile is at that position
+            Debug.Log(tilemap.GetTile(cellPos));  
+            //change the tile at that position
+            tilemap.SetTile(cellPos, flower);
+            impulse.GenerateImpulse();
+        }
     }
 }
