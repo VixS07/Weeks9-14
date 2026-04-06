4d3625ed5595c2c4245bc68ec7218ed727ca7ce1
Vix <vix.sak@gmail.com>
2026-04-05 21:28:46 -0400

    loop through the tilemaps, and calculate next location to figure out if the next movement will be on a wall tile, if it is dont go there

--- Assets/Projects/Systems Project/Player.cs
+++ Assets/Projects/Systems Project/Player.cs
@@ -1,12 +1,17 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
 
     Vector2 playerPos;
+    float speed = 5;
 
     public Vector2 movement;
+    Vector2 nextMovement;
     public GameObject cameraTracker;
 
     public Vector2 bottomLeft;
@@ -14,6 +19,11 @@ public class Player : MonoBehaviour
 
     public Vector2 roomSize;
 
+    
+    public List<Tilemap> walls;
+    Vector3Int nextCell;
+    Vector2 input;
+    bool wallThere;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,21 +37,58 @@ public class Player : MonoBehaviour
     // Update is called once per frame 
     void Update()
     {
+        //get screen size
         bottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 10));
         topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 10));
 
-        transform.position += (Vector3)movement * 5f * Time.deltaTime;
-        playerPos = transform.position;
-
-        if(playerPos.x > topRight.x || playerPos.x < bottomLeft.x || 
+        //call camera movement when player walks off the edge of the screen
+        if (playerPos.x > topRight.x || playerPos.x < bottomLeft.x || 
             playerPos.y > topRight.y || playerPos.y < bottomLeft.y)
         {
             moveCam();
+        }
+  
+        //player movement
+        //assign input
+        input = movement;
+
+        //check next positon
+        nextMovement = (Vector2)transform.position + input  * speed * Time.deltaTime;
+
+        //loop through all the tilemaps in the walls list
+        for (int i = 0; i < walls.Count; i++)
+        {
+            //get the cell of the next position in the current tilemap
+            nextCell = walls[i].WorldToCell(nextMovement);
+            Tile wallTile = (Tile)walls[i].GetTile(nextCell);
+            Debug.Log("Checking cell: " + nextCell + " | Tile: " + wallTile);
+
+            //checks if either tilemap has a wall tile in the next position, if it does then the player can't move
+            if (wallTile != null)
+            {
+                wallThere = true;
+                break;
+            }
+            else
+            {
+                wallThere = false;
+            }
+        }
+        if (wallThere)
+        {
+            movement = Vector2.zero;
+            Debug.Log("Wall in the way");
+        }
+        else
+        {
+            transform.position += (Vector3)movement * speed * Time.deltaTime;
+            playerPos = transform.position;
         }
     }
 
     public void moveCam() 
     {
+        //get the camera position from the camera tracker game object.
         Vector2 cameraPos = cameraTracker.transform.position; 
         //move camera to the right by the width of the screen when player moves off the right edge of the screen
         if (playerPos.x > topRight.x)
@@ -72,7 +119,9 @@ public class Player : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        //find out which way the player is trying to move and store it in a vector2
         movement = context.ReadValue<Vector2>();
-        
-    }
+
+        }
+
 }
