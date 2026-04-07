a086f1f141bb095f77700b8c8d479954ea8c07cf
Vix <vix.sak@gmail.com>
2026-04-06 23:44:03 -0400

    spike properly moves down using a coroutine

--- Assets/Projects/Systems Project/scripts/SpikeMove.cs
+++ Assets/Projects/Systems Project/scripts/SpikeMove.cs
@@ -5,7 +5,9 @@ public class SpikeMove : MonoBehaviour
 {
     public Transform spike;
     float height = 1;
-    float yPos;
+    float yPos = 0;
+    float moveAmount;
+    public float moveMargin;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,19 +25,32 @@ public class SpikeMove : MonoBehaviour
         Debug.Log("lowering...");
         spike.localScale = new Vector2 (1, height);
         //y .5 of a change
-        while (height > 0)
+        while (height > 0 || yPos < moveMargin)
         {
-            Debug.Log("being lowered...");
-            height -= 1 * Time.deltaTime / 2;
-            spike.localScale = new Vector2(1, height);
-            while (yPos > 0.5)
+            if (height > 0)
             {
-                yPos -= 1 * Time.deltaTime;
-                spike.localPosition = new Vector2(spike.localPosition.x, (spike.localPosition.y - yPos));
+                Debug.Log("being lowered...");
+                height -= 1 * Time.deltaTime / 2f;
+                if(height < 0)
+                {
+                    height = 0;
+                }
+                spike.localScale = new Vector2(1, height);
+            }
+
+            if (yPos < moveMargin)
+            {
+                moveAmount = 1 * Time.deltaTime / 4f;
+                yPos += moveAmount;
+                if(yPos > moveMargin)
+                {
+                    yPos = moveMargin;
+                }
+                spike.localPosition = new Vector2(spike.localPosition.x, (spike.localPosition.y - moveAmount));
             }
             yield return null;
         }
-        if(height <= 0)
+        if(height <= 0 && yPos >= moveMargin)
         {
             Debug.Log("done!");
         }
