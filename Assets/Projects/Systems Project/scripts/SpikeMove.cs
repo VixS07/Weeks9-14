4ba491dcfa668da4bff23b95ebc5392c60b76051
Vix <vix.sak@gmail.com>
2026-04-06 22:49:43 -0400

    started the coroutine, unity is unable to close, im  pushing so I dont lose progress resetting my computer

--- Assets/Projects/Systems Project/scripts/SpikeMove.cs
+++ Assets/Projects/Systems Project/scripts/SpikeMove.cs
@@ -1,16 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpikeMove : MonoBehaviour
 {
+    public Transform spike;
+    float height = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(IntoFloor());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator IntoFloor()
+    {
+        Debug.Log("lowering...");
+        spike.localScale = new Vector2 (1, height);
+
+        if (height > 0)
+        {
+            height += 1 * Time.deltaTime;
+            spike.localScale = new Vector2 (1, height);
+        }
+        if(height == 0)
+        {
+            Debug.Log("done!");
+        }
+        yield return null;
     }
 }
