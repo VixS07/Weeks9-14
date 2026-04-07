b4d2f338bf101ae7168028e143ee3b01a80aa5c1
Vix <vix.sak@gmail.com>
2026-04-06 23:15:53 -0400

    made the spike squish down

--- Assets/Projects/Systems Project/scripts/SpikeMove.cs
+++ Assets/Projects/Systems Project/scripts/SpikeMove.cs
@@ -5,6 +5,7 @@ public class SpikeMove : MonoBehaviour
 {
     public Transform spike;
     float height = 1;
+    float yPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,15 +22,20 @@ public class SpikeMove : MonoBehaviour
     {
         Debug.Log("lowering...");
         spike.localScale = new Vector2 (1, height);
-
-        if (height > 0)
+        //y .5 of a change
+        while (height > 0)
         {
-            Debug.Log("being lowered..."); 
-            height -= 1 * Time.deltaTime;
-            spike.localScale = new Vector2 (1, height);
+            Debug.Log("being lowered...");
+            height -= 1 * Time.deltaTime / 2;
+            spike.localScale = new Vector2(1, height);
+            while (yPos > 0.5)
+            {
+                yPos -= 1 * Time.deltaTime;
+                spike.localPosition = new Vector2(spike.localPosition.x, (spike.localPosition.y - yPos));
+            }
             yield return null;
         }
-        if(height == 0)
+        if(height <= 0)
         {
             Debug.Log("done!");
         }
